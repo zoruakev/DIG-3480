544ce6970e2d504d599d83fcd42241fcbb23cfca
zoruakev <42977695+zoruakev@users.noreply.github.com>
2018-10-31 00:23:58 -0400

    Hell

---
+++ Scripts/DestroybyContact.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroybyContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gamecontroller;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gamecontroller = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gamecontroller == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gamecontroller.GameOver();
+        }
+        gamecontroller.AddScore (scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
---
+++ Scripts/GameController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine (SpawnWaves());
+        score = 0;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        SetScoreText();
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
